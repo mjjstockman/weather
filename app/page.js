bfdfa18322485f5beae6ef2a0a7f6fadbcf1042a
mjjstockman <mjjstockman@gmail.com>
2023-10-10 16:45:28 +0100

    Add city name input tp page.js

--- app/page.js
+++ app/page.js
@@ -2,9 +2,9 @@
 import { useState, useEffect } from 'react';
 import ApiClient from '../ApiClient/client';
 import UIMessage from '@/components/typography/UIMessage';
+import AddLocation from '@/components/forms/AddLocation';
 
 export default function Home() {
-  const [suggestion, setSuggestion] = useState(null);
   const [isUIMessage, setIsUIMessage] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
   const [successMessage, setSuccessMessage] = useState(null);
@@ -12,28 +12,17 @@ export default function Home() {
 
   // instantiate new ApiClient
   const apiClient = new ApiClient();
+  <input
+    type='text'
+    placeholder='Type here'
+    className='input w-full max-w-xs'
+  />;
 
   const getData = async () => {
     try {
-      // const response = await apiClient.getWeather();        setWeather(
-      //     // response.data
-      //     // {
-      //   )`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${process.env.WEATHER_KEY}`
-      //   // `https://api.openweathermap.org/data/2.5/weather?q=sheffield=${process.env.WEATHER_KEY}`
-      // );
-
-      // get response from apiclient
-
       const response = await apiClient.getWeather();
-
       console.log(response);
-
-      // console.log(response)
-
-      // depending on response format, we set our state
-
       setSuccessMessage('success!!!!!!');
-      // setSuggestion(data.data);
     } catch (err) {
       setErrorMessage('ERRRRROOOOORRRRRRRRR');
       console.log(err);
@@ -58,8 +47,7 @@ export default function Home() {
       )}
       <main className='flex flex-col items-center justify-between p-24'>
         <h1 className='text-4xl font-bold'>Hello World</h1>
-        {suggestion?.activity}
-        <button className='btn btn-primary'>Primary</button>
+        <AddLocation />
       </main>
     </>
   );
---
+++ components/forms/AddLocation.js
@@ -0,0 +1,13 @@
+const AddLocation = () => {
+  return (
+    <form>
+      <input
+        type='text'
+        placeholder='Enter city name'
+        className='input w-full max-w-xs'
+      />
+    </form>
+  );
+};
+
+export default AddLocation;
