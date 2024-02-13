4c30e69841fb1a2c2fcc3f0a6e6a7ebaffc71a80
mjjstockman <mjjstockman@gmail.com>
2024-02-13 13:59:47 +0000

    Add basic project structure and set up Daisy UI

---
+++ app/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <button className='btn'>Button</button>
+      <button className='btn btn-neutral'>Neutral</button>
+      <button className='btn btn-primary'>Primary</button>
+      <button className='btn btn-secondary'>Secondary</button>
+      <button className='btn btn-accent'>Accent</button>
+      <button className='btn btn-ghost'>Ghost</button>
+      <button className='btn btn-link'>Link</button>
+    </main>
+  );
+}
